021fc38b7953646c0504e839d1fce9a191961ed9
Emmanuel Jazon <131515197+jzonx8@users.noreply.github.com>
2024-04-27 20:21:22 +0800

    Create MotorcycleSelectionActivity.java

---
+++ MotorcycleSelectionActivity.java
@@ -0,0 +1,17 @@
+package com.example.finalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MotorcycleSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_main);
+
+        // Additional code for motorcycle selection activity
+        // For example, display motorcycle options, user preferences, etc.
+    }
+}

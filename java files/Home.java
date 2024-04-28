fd0fc0a617b85abf663e47ae00412f6b1eeb1c30
Emmanuel Jazon <131515197+jzonx8@users.noreply.github.com>
2024-04-28 21:39:07 +0800

    Update Home.java

---
+++ java files/Home.java
@@ -0,0 +1,18 @@
+
+package com.example.finalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_main);
+
+        // Additional code for motorcycle selection activity
+        // For example, display motorcycle options, user preferences, etc.
+    }
+}

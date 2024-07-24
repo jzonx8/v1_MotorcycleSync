55458a22cba8e07c8ee9d563ca8d1c2dbb8a2844
Emmanuel Jazon <131515197+jzonx8@users.noreply.github.com>
2024-07-24 08:31:29 +0800

    Delete java directory

--- java/DrawerActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.finalproject;
-
-import android.app.Activity;
-
-public class DrawerActivity extends Activity {
-}
--- java/SettingActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.finalproject;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-public class SettingActivity extends AppCompatActivity {
-
-}
